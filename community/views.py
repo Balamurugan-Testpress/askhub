9d4163505aaa2cf9ae60862fd56eb0ad1d0bb252
Balamurugan-Testpress <balamurugan@testpress.in>
2025-07-22 15:58:10 +0530

    refactor: use django-filter for question list filtering by search and tags

--- community/views.py
+++ community/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-<<<<<<< HEAD
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404
 from django.urls import reverse, reverse_lazy
@@ -10,12 +9,6 @@ from community.forms import AnswerForm, QuestionForm
 
 from .filters import QuestionFilter
 from .models import Answer, Question
-=======
-from django.views.generic import ListView
-from django.db.models import Q
-from .models import Question
-from taggit.models import Tag
->>>>>>> 6cd0cb5 (feat: implement and test search and filter by tags)
 
 
 class QuestionListView(LoginRequiredMixin, ListView):
@@ -117,27 +110,12 @@ class QuestionListView(LoginRequiredMixin, ListView):
     paginate_by = 10
 
     def get_queryset(self):
-        query = self.request.GET.get("q")
-        tag_param = self.request.GET.get("tag", "").strip()
-        queryset = Question.objects.all()
-        if query:
-            queryset = queryset.filter(
-                Q(title__icontains=query) | Q(description__icontains=query)
-            )
-        if tag_param:
-            tag_list = [tag.strip() for tag in tag_param.split(",") if tag.strip()]
-            for tag in tag_list:
-                queryset = queryset.filter(tags__name__iexact=tag)
-
-        return queryset.order_by("-created_at")
+        queryset = super().get_queryset()
+        self.filterset = QuestionFilter(self.request.GET, queryset=queryset)
+        return self.filterset.qs.distinct()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["all_tags"] = Tag.objects.all()
-
-        tag_param = self.request.GET.get("tag", "")
-        selected_tags = [tag.strip() for tag in tag_param.split(",") if tag.strip()]
-        context["selected_tags"] = selected_tags
-
-        context["search_query"] = self.request.GET.get("q", "")
+        context["filterset"] = self.filterset
         return context
