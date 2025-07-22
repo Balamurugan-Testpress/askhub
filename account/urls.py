cbc5310778cd9358448a34e04a0f09c4b292d050
Balamurugan-Testpress <balamurugan@testpress.in>
2025-07-22 11:18:03 +0530

    feat: implement and test user authentication flow (login, logout, register and password reset)
    * feat: implement and test user authentication flow (login, logout, register)
    
    * style: apply Bootstrap styling to all authentication templates
    
    * refactor(account): convert register view from FBV to CBV
    
    * refactor(account): convert register class from View to FormView

---
+++ account/urls.py
@@ -0,0 +1,41 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import Register
+
+urlpatterns = [
+    path("register/", Register.as_view(), name="register"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="account/login.html"),
+        name="login",
+    ),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="account/password_reset.html"
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="account/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="account/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="account/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
+]
---
+++ account/views.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import FormView
+
+
+class Register(FormView):
+    template_name = "account/register.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
