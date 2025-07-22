f4d48258cd886dc1034b41a5f1d01be97db2121b
Balamurugan-Testpress <balamurugan@testpress.in>
2025-07-22 12:45:17 +0530

    refactor: list is not properly ordering and clean views.py

--- community/views.py
+++ community/views.py
@@ -1,21 +1,13 @@
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 
 from community.models import Question
 
 
-@login_required
-def home(request):
-    return HttpResponse(f"Welcome home {request.user}")
-
-
 class QuestionList(LoginRequiredMixin, ListView):
     model = Question
     template_name = "community/question_list.html"
-    # context_object_name = "question_list"
     paginate_by = 10
 
-    class Meta:
-        ordering = ("-created_at",)
+    def get_queryset(self):
+        return Question.objects.order_by("-created_at")
