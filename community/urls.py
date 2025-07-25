b4b502b8573a84f75f4556320eeeabc4d94e69f4
Balamurugan R <balamurugan@testpress.in>
2025-07-25 12:52:43 +0530

    feat: implement question detail page and answer detail page (#6)
    * feat: implement and test question list view
    
    * refactor: list is not properly ordering and clean views.py
    
    * refactor: change view class name
    
    * feat: implement and test search and filter by tags
    
    * refactor: use django-filter for question list filtering by search and tags
    
    * feat: implement question detail page and answer detail page
    
    * refactor: change pk to answer_id in url.py

--- community/urls.py
+++ community/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from community.views import QuestionListView
+from community.views import AnswerDetailView, QuestionDetailView, QuestionListView
 
 urlpatterns = [
     path("", QuestionListView.as_view(), name="question_list"),
+    path("question/<int:pk>/", QuestionDetailView.as_view(), name="question_detail"),
+    path(
+        "question/<int:question_id>/answer/<int:answer_id>/",
+        AnswerDetailView.as_view(),
+        name="answer_detail",
+    ),
 ]
