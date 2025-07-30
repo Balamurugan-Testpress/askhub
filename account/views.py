313008b49da14d5d1eac8d37c00a38095de263f6
Balamurugan-Testpress <balamurugan@testpress.in>
2025-07-30 09:55:08 +0530

    fix redirect after register

--- account/views.py
+++ account/views.py
@@ -7,7 +7,7 @@ from django.views.generic import FormView
 class Register(FormView):
     template_name = "account/register.html"
     form_class = UserCreationForm
-    success_url = reverse_lazy("login")
+    success_url = reverse_lazy("question_list")
 
     def form_valid(self, form):
         user = form.save()
